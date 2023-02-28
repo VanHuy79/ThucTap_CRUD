fe707705d4d8985a549cbea2a7ecdce3a93ae0ee
VanHuy79 <ngo.huy.van.2001@gmail.com>
2023-02-28 22:23:29 +0700

    update Service

--- CRUD/app/Service/Post/PostServiceInterface.php
+++ CRUD/app/Service/Post/PostServiceInterface.php
@@ -6,4 +6,9 @@ use App\Service\ServiceInterface;
 
 interface PostServiceInterface extends ServiceInterface
 {
+   public function getAll();
+   public function createPost($request);
+   public function showPost($id);
+   public function updatePost($request, $id);
+   public function deletePost($id);
 }
