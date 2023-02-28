631f586a03c9d9ccd6c9d26ace62642d609529bb
VanHuy79 <ngo.huy.van.2001@gmail.com>
2023-02-28 22:22:33 +0700

    update Service

--- CRUD/app/Service/Post/PostService.php
+++ CRUD/app/Service/Post/PostService.php
@@ -5,6 +5,7 @@ namespace App\Service\Post;
 use App\Repositories\Post\PostRepositoryInterface;
 
 use App\Service\BaseService;
+
 // Kế thừa các phương thức của Base
 class PostService extends BaseService implements PostServiceInterface
 {
@@ -14,5 +15,95 @@ class PostService extends BaseService implements PostServiceInterface
    public function __construct(PostRepositoryInterface $postRepository)
    {
       $this->repository = $postRepository;
+   }
+   public function getAll()
+   {
+      $data = $this->repository->all();
+
+      return response()->json([
+         'success' => true,
+         'message' => 'List danh sách Post',
+         'data' => $data,
+      ]);
+   }
+
+   public function createPost($request)
+   {
+      $params = [
+         'name' => $request->name,
+         'description' => $request->description,
+         'field_image' => $request->field_image,
+         'user_id' => 1,
+      ];
+
+      $data = $this->repository->create($params);
+
+      if ($data) {
+         return response()->json([
+            'success' => true,
+            'message' => 'Thêm thành công',
+            'data' => $data,
+         ]);
+      }
+      return response()->json([
+         'success' => false,
+         'message' => 'Thêm không thành công',
+      ]);
+   }
+
+   public function showPost($id)
+   {
+      $data = $this->repository->find($id);
+
+      if ($data) {
+         return response()->json([
+            'success' => true,
+            'data' => $data
+         ]);
+      }
+      return response()->json([
+         'success' => false,
+         'message' => 'Không tìm thấy Post cần tìm',
+      ]);
+   }
+
+   public function updatePost($request, $id)
+   {
+      $params = [
+         'name' => $request->name,
+         'description' => $request->description,
+         'field_image' => $request->field_image,
+         'user_id' => 1,
+      ];
+
+      $data = $this->repository->update($params, $id);
+
+      if ($data) {
+         return response()->json([
+            'success' => true,
+            'message' => 'Cập nhật thành công',
+            'data' => $data,
+         ]);
+      }
+      return response()->json([
+         'success' => false,
+         'message' => 'Không tìm thấy Post cần cập nhật',
+      ]);
+   }
+
+   public function deletePost($id)
+   {
+      $data = $this->repository->delete($id);
+
+      if ($data) {
+         return response()->json([
+            'success' => true,
+            'message' => 'Xóa thành công',
+         ]);
+      }
+      return response()->json([
+         'success' => false,
+         'message' => 'Không tìm thấy Post cần xóa',
+      ]);
    }
 }
